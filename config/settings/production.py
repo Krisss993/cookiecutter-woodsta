3c082876db00ad6f00f1cc0dda1f9e74c13ac429
krisss <kristopherp994@gmail.com>
2025-05-13 14:54:32 +0200

    Form added

--- config/settings/production.py
+++ config/settings/production.py
@@ -85,7 +85,7 @@ STORAGES = {
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="sta <noreply@https://woodsta.onrender.com>",
+    default="sta <noreply@woodsta.onrender.com>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
